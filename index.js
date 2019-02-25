01b336a35ceaf808dd8f1e695a533612ed7c7bf4
LeannePepper <leanne.werner.lw@gmail.com>
2019-02-25 15:14:24 -0800

    added options for time zones and message error states

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const alertButton = document.querySelector(".alert-close"); 
+const peopleSearch = document.querySelector(".search-user"); 
+const people = ['victoria chambers','dale byrd', 'dawn wood', 'dan oliver', 'jane doe'];
+const messagesDiv = document.querySelector(".messages");
+const message = document.querySelector('textarea'); 
+const sendButton = document.querySelector('#message-send');
 
+// functionality to close the alert box 
 alertButton.addEventListener('click', function(){
     const alertModal= document.querySelector('.alert-modal'); 
     alertModal.style.display = "none";
 })
+
+
+// ***********CHARTS****************************
 
 // Line Chart
 const lineChart = document.getElementById('line').getContext('2d');
@@ -86,3 +95,92 @@ const pieChart = new Chart(pie, {
         responsive: true,
       }
 });
+
+//*************SEARCH BAR FUNCTIONALITY *****************/
+
+
+
+// function searchPeople(inp, arr) {
+//     var currentFocus; 
+
+//     inp.addEventListener("input", function(e){
+//         var a, b, i, val = this.value; 
+
+//         closeAllList();
+//         if(!val) {
+//             return false;
+//         }
+//         currentFocus = -1; 
+//         a = document.createElement("DIV"); 
+//         a.setAttribute("id", "autocomplete-items");
+//         this.parentNode.appendChild(a); 
+
+//         for(i = 0; i < arr.length; i++){
+//             if(arr[i].substr(0, val.length).toUpperCase == val.toUpperCase()){
+//                 b = document.createElement("DIV");
+//                 b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+//                 b.innerHTML += arr[i].substr(val.length);
+//                 b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+
+//                 b.addEventListener("click", function(e){
+//                     inp.value = this.getElementsByTagName("input")[0].value;
+//                     closeAllLists();
+//                 });
+//                 a.appendChild(b); 
+//             }
+//         }
+//     });
+//     inp.addEventListener("keydown", function(e) {
+//         var x = document.getElementById(this.id + "autocomplete-list");
+//         if (x) x = x.getElementsByTagName("div");
+//         if (e.keyCode == 40) {
+//             currentFocus++;
+//             addActive(x);
+//       } else if (e.keyCode == 38) {
+//         currentFocus--;
+//         addActive(x);
+//       } else if (e.keyCode == 13) {
+//         e.preventDefault();
+//         if (currentFocus > -1) {
+//             if (x) x[currentFocus].click();
+//         }
+//       }
+//   });
+// }
+ 
+// ************ Message sent functions and resets ******************
+
+function createSuccessMessage() {
+    const successMessage = document.createElement('DIV'); 
+    successMessage.innerHTML = "Your message was sent!";
+    successMessage.setAttribute('class', 'successMessage'); 
+    messagesDiv.appendChild(successMessage); 
+
+    setTimeout(function(){
+        messagesDiv.removeChild(successMessage); 
+        peopleSearch.value = '';
+        message.value = '';
+    }, 3000); 
+}
+function createFailMessage() {
+    const failMessage = document.createElement('DIV'); 
+    failMessage.innerHTML = "Ops! Looks like one or more of the fields has not been completed"; 
+    failMessage.setAttribute('class', 'failMessage'); 
+    messagesDiv.appendChild(failMessage); 
+
+    setTimeout(function(){
+        messagesDiv.removeChild(failMessage); 
+        peopleSearch.value = '';
+        message.value = '';
+    }, 3000); 
+}
+
+sendButton.addEventListener('click', function(){
+    if(peopleSearch.value === '' || message.value === '') {
+        createFailMessage();
+    } else {
+        createSuccessMessage(); 
+    }
+})
+
+// *********** Notifications **************
